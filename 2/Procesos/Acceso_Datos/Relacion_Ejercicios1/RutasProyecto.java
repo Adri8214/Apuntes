1ed4f68c1e8e2b31a21372773895b64aeeeaa5c8
verdusanchezadrian <verdusanchezadrian001.alu@stemgranada.com>
2026-09-24 11:15:41 +0200

    Ejercicio 1 de la rela relación 1 de ejercicios

--- 2/Procesos/Acceso_Datos/Relacion_Ejercicios1/RutasProyecto.java
+++ 2/Procesos/Acceso_Datos/Relacion_Ejercicios1/RutasProyecto.java
@@ -10,19 +10,19 @@ import java.nio.file.Path;
 public class RutasProyecto {
     public static void main(String[] args) {
 
-        Path carpeta = Path.of("datos");
-        Path fichero = carpeta.resolve("clubes.txt");
-        Path copias = carpeta.resolve("copias");
+        Path datos = Path.of("datos");
+        Path fichero = datos.resolve("clubes.txt");
+        Path copias = datos.resolve("copias");
 
         System.out.println("--- RUTAS ABSOLUTAS --- ");
-        System.out.println("Ruta1: " + carpeta.toAbsolutePath());
+        System.out.println("Ruta1: " + datos.toAbsolutePath());
         System.out.println("Ruta2: " + fichero.toAbsolutePath());
         System.out.println("Ruta3: " + copias.toAbsolutePath());
 
         System.out.println();
 
         System.out.println("--- RUTAS RELATIVAS --- ");
-        System.out.println("Ruta1: " + carpeta);
+        System.out.println("Ruta1: " + datos);
         System.out.println("Ruta2: " + fichero);
         System.out.println("Ruta3: " + copias);
     }
