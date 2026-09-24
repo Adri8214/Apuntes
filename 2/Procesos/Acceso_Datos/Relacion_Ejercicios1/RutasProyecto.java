6e9e1524a1e50839f387d8dced2206a156b02bdd
verdusanchezadrian <verdusanchezadrian001.alu@stemgranada.com>
2026-09-24 11:09:24 +0200

    Ejercicio 1

---
+++ 2/Procesos/Acceso_Datos/Relacion_Ejercicios1/RutasProyecto.java
@@ -0,0 +1,29 @@
+// Crea una clase RutasProyecto que represente las rutas de la carpeta datos , el fichero clubes.txt y una subcarpeta llamada copias . No debes crear todavía ningún archivo ni carpeta.
+// Construye la ruta de datos con Path.of(...) .
+// Obtén las otras dos rutas utilizando resolve(...) .
+// Muestra en consola las tres rutas relativas y sus correspondientes rutas absolutas con toAbsolutePath() .
+// Comprueba: observa que construir o mostrar una ruta no crea por sí mismo un archivo. 
+// Ejecuta la clase desde el directorio de trabajo de tu proyecto.
+
+import java.nio.file.Path;
+
+public class RutasProyecto {
+    public static void main(String[] args) {
+
+        Path carpeta = Path.of("datos");
+        Path fichero = carpeta.resolve("clubes.txt");
+        Path copias = carpeta.resolve("copias");
+
+        System.out.println("--- RUTAS ABSOLUTAS --- ");
+        System.out.println("Ruta1: " + carpeta.toAbsolutePath());
+        System.out.println("Ruta2: " + fichero.toAbsolutePath());
+        System.out.println("Ruta3: " + copias.toAbsolutePath());
+
+        System.out.println();
+
+        System.out.println("--- RUTAS RELATIVAS --- ");
+        System.out.println("Ruta1: " + carpeta);
+        System.out.println("Ruta2: " + fichero);
+        System.out.println("Ruta3: " + copias);
+    }
+}
