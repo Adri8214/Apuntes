77136526b3a2b8596ecc1eb15f0496953efc7dc9
Adri8214 <asnchz18@gmail.com>
2026-09-25 12:16:22 +0200

    Ejercicio 4

---
+++ 2/Procesos/Acceso_Datos/Relacion_Ejercicios1/ListarDatos.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ListarDatos {
+    public static void main(String[] args) {
+        
+        Path carpeta = Path.of("datos");
+        
+
+        try (DirectoryStream<Path> elementos = Files.newDirectoryStream(carpeta)) {
+            
+            for (Path elemento : elementos) {
+                if (Files.isRegularFile(elemento)) {
+                    System.out.println(elemento.getFileName());
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println("No se ha podido acceder al archivo " + e.getMessage());
+        }
+    }
+}
