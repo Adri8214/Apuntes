6a9d0f6ffa294c46dc27c9c262cbe30a1ef8e690
Adri8214 <asnchz18@gmail.com>
2026-09-25 18:06:54 +0200

    Ejercicios 5 y 8

--- 2/Procesos/Acceso_Datos/Relacion_Ejercicios1/PrepararCarpetas.java
+++ 2/Procesos/Acceso_Datos/Relacion_Ejercicios1/PrepararCarpetas.java
@@ -12,7 +12,7 @@ public class PrepararCarpetas {
     public static void main(String[] args) {
         
         Path carpeta1 = Path.of("datos");
-        Path carpeta2 = Path.of("datos/copias");
+        Path carpeta2 = carpeta1.resolve("copias");
 
         Path archivo1 = carpeta1.resolve("clubes.txt");
         Path archivo2 = carpeta2.resolve("respaldo.txt");
